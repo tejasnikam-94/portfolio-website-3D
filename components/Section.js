e7db16672c0c265d045bb0c5e8104b4b14e71c2a
tejasnikam-94 <nextgen.lab26@gmail.com>
2026-06-13 16:29:25 +0530

    Delete components directory

--- components/Section.js
+++
@@ -1,8 +0,0 @@
-
-export default function Section({title,text}){
-  return (
-  <section style={{padding:'80px 10%',borderTop:'1px solid #222'}}>
-    <h2>{title}</h2>
-    <p>{text}</p>
-  </section>)
-}
